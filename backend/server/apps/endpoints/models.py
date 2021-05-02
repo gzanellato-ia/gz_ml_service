64d35f1dd38d85eeb3ab863cb8a298cc28004de9
gzanellato-ia <gzanellato@hotmail.com>
2021-05-02 14:53:46 -0300

    ab tests

--- backend/server/apps/endpoints/models.py
+++ backend/server/apps/endpoints/models.py
@@ -71,6 +71,31 @@ class MLRequest(models.Model):   # to keep information about all requests to ML 
     '''
     input_data = models.CharField(max_length=10000)
     full_response = models.CharField(max_length=10000)
+
+
+# please add at the end of file backend/server/apps/endpoints/models.py
+
+class ABTest(models.Model):
+    '''
+    The ABTest will keep information about A/B tests.
+    Attributes:
+        title: The title of test.
+        created_by: The name of creator.
+        created_at: The date of test creation.
+        ended_at: The date of test stop.
+        summary: The description with test summary, created at test stop.
+        parent_mlalgorithm_1: The reference to the first corresponding MLAlgorithm.
+        parent_mlalgorithm_2: The reference to the second corresponding MLAlgorithm.
+    '''
+    title = models.CharField(max_length=10000)
+    created_by = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    ended_at = models.DateTimeField(blank=True, null=True)
+    summary = models.CharField(max_length=10000, blank=True, null=True)
+
+    parent_mlalgorithm_1 = models.ForeignKey(MLAlgorithm, on_delete=models.CASCADE, related_name="parent_mlalgorithm_1")
+    parent_mlalgorithm_2 = models.ForeignKey(MLAlgorithm, on_delete=models.CASCADE, related_name="parent_mlalgorithm_2")
+
     response = models.CharField(max_length=10000)
     feedback = models.CharField(max_length=10000, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
